d1823b11a5baeeca230d8b6b3a197d861894ef80
Claudia Scarbrough <rubyharrts@gmail.com>
2021-10-15 13:33:44 -0500

    11 passed

--- index.js
+++ index.js
@@ -91,8 +91,11 @@ const zooAnimals = [
   */
 
 
-  function lowPopulationAnimals(array){
-    const lowPops = array.filter(zooAnimals => zooAnimals.population < 5);
+  function lowPopulationAnimals(data){
+    const lowPops = data.filter(function(item){
+      return item.population < 5;
+    })
+    return lowPops;
   }
   
 
@@ -102,9 +105,14 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+  function USApop(data){
+    const totalPop = zooAnimals.reduce(function(acc, item){
+      return acc + item.population;
+    }, 0);
+    return totalPop;
+   }
+ 
+ console.log(USApop(zooAnimals));
   
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
@@ -115,30 +123,30 @@ const zooAnimals = [
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
+  function consume(a, b, cb){
+    return cb(a, b);
   }
  
   
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
+function add(a, b){
+    return a + b;
   }
 
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
   
-function multiply(/*Your Code Here */){
-   /*Your Code Here */
+function multiply(a, b){
+   return a * b;
   }
 
 
  // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
   
-function greeting(/*Your Code Here */){
-   return /*Your Code Here */
+function greeting(first, last){
+   return `Hello ${first} ${last}, nice to meet you!`
   }
   
   
